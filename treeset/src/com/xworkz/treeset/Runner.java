7a1213ef9ebf9de5667f90c5bb80afe2f5b7947e
ulaga774 <ulaga774xworkz@gmail.com>
2023-06-05 15:02:39 +0530

    NewPush

---
+++ treeset/src/com/xworkz/treeset/Runner.java
@@ -0,0 +1,31 @@
+package com.xworkz.treeset;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Runner {
+	
+
+	public static void main(String[] args) {
+		
+		
+		
+
+HashMap<Integer, String> obj = new HashMap<Integer, String> ();
+obj.put(1, "Kiran");
+obj.put(1, "Suheb");
+ System.out.println(obj.get(1));
+}
+}
+		
+		
+		
+		
+		
+
+  	
+			
+	
